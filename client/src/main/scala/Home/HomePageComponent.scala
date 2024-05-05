7da3b4c133b900f9b43bb3c57407f6477de756da
LeviKle <leuykhiem@gmail.com>
2024-05-05 04:50:13 -0500

    added nav bar

---
+++ client/src/main/scala/Home/HomePageComponent.scala
@@ -0,0 +1,40 @@
+package playscala
+
+import slinky.core.annotations.react
+import slinky.core.Component
+import slinky.core.facade.ReactElement
+import slinky.web.html.div
+
+import org.scalajs.dom.document
+import org.scalajs.dom.html
+import org.scalajs.dom.window
+import slinky.web.html._
+import models.UserChats
+import models.ReadsAndWrites._
+
+@react class HomePageComponent extends Component {
+  type Props = Unit
+  case class State(page: String)
+  def initialState: State = State("Chat")
+
+  implicit val ec = scala.concurrent.ExecutionContext.global
+
+  lazy val chatPageComponent: ReactElement = ChatPageComponent()
+  val pages: Map[String, ReactElement] =  Map("Matching" -> chatPageComponent, "Profile" -> chatPageComponent, "Chat" -> chatPageComponent)
+
+  def selectPage(): ReactElement = pages.get(state.page)
+
+  def render(): ReactElement = {
+    div( id:="homePage",
+      div( id:="navbar",
+        div( id := "pageButtonsContainer",
+          pages.keySet.zipWithIndex.map { case (pageName, i) =>
+            button(key := i.toString(), className := "pageButton", id := {if (pageName == state.page) "selectedButton" else ""}, onClick := (e => {setState(state.copy(page = pageName))}), pageName)
+          }
+        )
+      ),
+      selectPage()
+    )
+  }
+
+}
--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -12,14 +12,13 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    // What is below is using Scala.js with Slinky to use React.
-    if (dom.document.getElementById("chatPage") != null) {
-      println("Rendering chat page")
-      ReactDOM.render(
-        ChatPageComponent(),
-        dom.document.getElementById("react-root")
-      )
-    }
+    // if (dom.document.getElementById("chatPage") != null) {
+    //   println("Rendering chat page")
+    //   ReactDOM.render(
+    //     ChatPageComponent(),
+    //     dom.document.getElementById("react-root")
+    //   )
+    // }
 
     if(dom.document.getElementById("authentication-page") != null){
       println("Rendering authentication page")
@@ -28,14 +27,21 @@ object Main {
         dom.document.getElementById("react-root")
       )
     }
-  }
 
-  if (dom.document.getElementById("onboarding-page") != null) {
-    println("Rendering onboarding page")
-    ReactDOM.render(
-      OnboardingComponent(),
-      dom.document.getElementById("react-root")
-    )
-  }
+    if (dom.document.getElementById("onboarding-page") != null) {
+      println("Rendering onboarding page")
+      ReactDOM.render(
+        OnboardingComponent(),
+        dom.document.getElementById("react-root")
+      )
+    }
 
+   if (dom.document.getElementById("home-page") != null) {
+      println("Rendering home page")
+      ReactDOM.render(
+        HomePageComponent(),
+        dom.document.getElementById("react-root")
+      )
+    }
+  }
 }
