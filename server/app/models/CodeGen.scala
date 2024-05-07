20bf2fc3300c6fcd4f43e3e3b2a7b46a11f028ea
LeviKle <leuykhiem@gmail.com>
2024-05-07 00:09:40 -0500

    fixed tables

--- server/app/models/CodeGen.scala
+++ server/app/models/CodeGen.scala
@@ -4,7 +4,7 @@ object CodeGen extends App {
   slick.codegen.SourceCodeGenerator.run(
     "slick.jdbc.PostgresProfile", 
     "org.postgresql.Driver",
-    "jdbc:postgresql://localhost:5432/dating?user=choudhry347&password=password",
+    "jdbc:postgresql://localhost:5432/dating?user=levy_betsy&password=password",
     "./server/app/", 
     "models", None, None, true, false
   )
